a9181882900f6ebcf3bec83cba8757635692bd01
LachieJK <lk77.external@pm.me>
2023-11-19 15:54:40 +1100

    Final Draft 1

--- project-2/commerce/auctions/urls.py
+++ project-2/commerce/auctions/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("index", views.index, name="index"),
     path("create", views.create, name="create"),
     path("category/<int:category_id>/", views.category, name="category"),
-    path("error/<str:message>", views.error, name="error"),
     path("listing/<int:listing_id>/", views.listing, name="listing"),
     path("watchlist/<int:user_id>/", views.watchlist, name="watchlist"),
     path("login", views.login_view, name="login"),
--- project-2/commerce/auctions/views.py
+++ project-2/commerce/auctions/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from decimal import Decimal
+from django.contrib import messages
 
 from .models import User, Listings, Category, Bids, Comments
 
 def open(request):
     return redirect('index');
+
 
 def index(request):
     return render(request, "auctions/index.html", {
@@ -16,14 +18,13 @@ def index(request):
         "categories": Category.objects.all().order_by('name')
     })
 
+
 def create(request):
     if request.method == "GET":
         return render(request, "auctions/create.html", {
             "categories": Category.objects.all().order_by('name')
         })
     else: 
-        if not request.user.is_authenticated:
-            return redirect('error', message="Must be signed in to create a listing")
         title = request.POST.get('title')
         description = request.POST.get('description')
         price = request.POST.get('price')
@@ -32,11 +33,16 @@ def create(request):
         if not image.strip():
             image = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.stack.imgur.com%2Fy9DpT.jpg&f=1&nofb=1&ipt=65fd893c5b83c58d8fa2af05c63703caffeecceb1e383b5b7c7fccc73fed228d&ipo=images'
         if not title.strip() or not description.strip() or not price.strip() or not category_name.strip():
-            return redirect('error', message="Not all required fields were filled correctly")
+            messages.success(request, "Not all required fields were filled correctly")
+            return render(request, "auctions/create.html", {
+                "categories": Category.objects.all().order_by('name')
+            })
         category_instance = Category.objects.get(name=category_name)
         listing = Listings(name = title, description = description, category = category_instance, owner = request.user, price = price, image = image)
         listing.save()
-        return redirect('index');
+        messages.success(request, "New listing was created and added to Active Listings")
+        return redirect('listing', listing.id);
+
 
 def category(request, category_id):
     return render(request, "auctions/category.html", {
@@ -45,8 +51,10 @@ def category(request, category_id):
         "title": Category.objects.get(id=category_id).name
     })
 
+
 def listing(request, listing_id):
     if request.method == 'GET':
+        success = True;
         listing = get_object_or_404(Listings, pk = listing_id)
         watched_by_ids = listing.watched_by.values_list('id', flat=True)
         return render(request, "auctions/listing.html", {
@@ -54,7 +62,8 @@ def listing(request, listing_id):
             "categories": Category.objects.all().order_by('name'),
             "comments": Comments.objects.filter(item=listing_id).order_by('-date'),
             "comments_count": Comments.objects.filter(item=listing_id).count(),
-            "watched_by_ids": watched_by_ids
+            "watched_by_ids": watched_by_ids,
+            "success": success
         })
     else:
         listing = get_object_or_404(Listings, pk = listing_id)
@@ -63,38 +72,76 @@ def listing(request, listing_id):
             current_user = request.user.id;
             listing = Listings.objects.get(id=listing_id)
             listing.watched_by.add(current_user)
+            success = True
+            messages.success(request, "Listing has been added to watchlist")
         if "remove-watchlist" in request.POST:
             current_user = request.user.id;
             listing = Listings.objects.get(id=listing_id)
             listing.watched_by.remove(current_user)
+            success = True
+            messages.success(request, "Listing has been removed from watchlist")
         if "bid" in request.POST:
             price_str = request.POST.get('bid')
             if not price_str.strip():
-                return redirect('error', message="Must enter a numeric bid value")
-            try:
-                price = Decimal(price_str)
-            except ValueError:
-                return redirect('error', message="Invalid bid value")
+                success = False
+                messages.success(request, "Must enter a numeric bid value")
+                return render(request, "auctions/listing.html", {
+                    "listing": listing,
+                    "categories": Category.objects.all().order_by('name'),
+                    "comments": Comments.objects.filter(item=listing_id).order_by('-date'),
+                    "comments_count": Comments.objects.filter(item=listing_id).count(),
+                    "watched_by_ids": watched_by_ids,
+                    "success": success
+                })
+            price = Decimal(price_str)
             if price <= Listings.objects.get(id=listing_id).current_price():
-                return redirect('error', message="Bid must be larger than the current price")
+                success = False
+                messages.success(request, "Bid must be larger than the current price")
+                return render(request, "auctions/listing.html", {
+                    "listing": listing,
+                    "categories": Category.objects.all().order_by('name'),
+                    "comments": Comments.objects.filter(item=listing_id).order_by('-date'),
+                    "comments_count": Comments.objects.filter(item=listing_id).count(),
+                    "watched_by_ids": watched_by_ids,
+                    "success": success
+                })
             bid = Bids(user = request.user, item = Listings.objects.get(id=listing_id), new_price = price)
             bid.save()
+            success = True
+            messages.success(request, "You now have the highest bid for this listing")
         if "close-listing" in request.POST:
             listing = Listings.objects.get(id=listing_id)
             listing.active = False
             listing.save()
+            success = True
+            messages.success(request, "Your listing is now closed")
         if "comments" in request.POST:
             description = request.POST.get('comments')
+            if not description.strip():
+                success = False
+                messages.success(request, "Must type in the prompt to post a comment")
+                return render(request, "auctions/listing.html", {
+                    "listing": listing,
+                    "categories": Category.objects.all().order_by('name'),
+                    "comments": Comments.objects.filter(item=listing_id).order_by('-date'),
+                    "comments_count": Comments.objects.filter(item=listing_id).count(),
+                    "watched_by_ids": watched_by_ids,
+                    "success": success
+                })
             comment = Comments(user = request.user, item = Listings.objects.get(id=listing_id), description = description)
             comment.save()
+            success = True
+            messages.success(request, "Your comment has now been added")
         return render(request, "auctions/listing.html", {
             "listing": listing,
             "categories": Category.objects.all().order_by('name'),
             "comments": Comments.objects.filter(item=listing_id).order_by('-date'),
             "comments_count": Comments.objects.filter(item=listing_id).count(),
-            "watched_by_ids": watched_by_ids
+            "watched_by_ids": watched_by_ids,
+            "success": success
         })
     
+
 def watchlist(request, user_id):
     if request.method == 'GET':
         return render(request, "auctions/watchlist.html", {
@@ -105,16 +152,12 @@ def watchlist(request, user_id):
         listing_id = request.POST.get('listing_id')
         listing = Listings.objects.get(id=listing_id)
         listing.watched_by.remove(user_id)
+        messages.success(request, "Listing was removed from watchlist.")
         return render(request, "auctions/watchlist.html", {
             "listings": Listings.objects.filter(watched_by=user_id).order_by('name'),
             "categories": Category.objects.all().order_by('name')
         })
 
-
-def error(request, message):
-    return render(request, "auctions/error.html", {
-        "message": message
-    }) 
 
 def login_view(request):
     if request.method == "POST":
@@ -127,11 +170,11 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
+            messages.success(request, "You are now logged in.")
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
+            messages.success(request, "Invalid username and/or password.")
+            return render(request, "auctions/login.html")
     else:
         return render(request, "auctions/login.html", {
             "categories": Category.objects.all().order_by('name')
@@ -140,6 +183,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
+    messages.success(request, "You are now logged out.")
     return HttpResponseRedirect(reverse("index"))
 
 
@@ -152,19 +196,18 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
+            messages.success(request, "Passwords must match.")
+            return render(request, "auctions/register.html")
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
+            messages.success(request, "Username already taken.")
+            return render(request, "auctions/register.html")
         login(request, user)
+        messages.success(request, "User has been created, and you are now logged in.")
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html", {
