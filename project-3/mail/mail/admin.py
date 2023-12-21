a964e712a477096afe7417af7257f53ecc3d8c34
LachieJK <lk77.external@pm.me>
2023-12-22 10:23:12 +1100

    Clear Folder

--- project-3/mail/mail/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Email
-
-# All of my models registered for the Django Admin Interface
-admin.site.register(User)
-admin.site.register(Email)
