ff6e54e9f136f9afe1c55cc62534eb513c0f4aff
LachieJK <lk77.external@pm.me>
2023-12-22 10:20:35 +1100

    Clear Folder

--- project-2/commerce/auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Category, Listings, Bids, Comments
-
-# All of my models registered for the Django Admin Interface
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Listings)
-admin.site.register(Bids)
-admin.site.register(Comments)
