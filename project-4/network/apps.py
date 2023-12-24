80465a92f9db10428a8390d16920433ef354bd34
LachieJK <lk77.external@pm.me>
2023-12-24 12:33:28 +1100

    Working Auto Profile Creation

--- project-4/network/apps.py
+++ project-4/network/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class NetworkConfig(AppConfig):
     name = 'network'
     
+    def ready(self):
+        import network.models  # Import your models to ensure signal handlers are connected
--- project-4/network/models.py
+++ project-4/network/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
@@ -45,3 +47,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user} commented on {self.post.user.username}'s post at {self.timestamp}"
+
+
+# Signal handlers
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
